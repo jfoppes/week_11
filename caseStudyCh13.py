2159ec1c1cde092e336c28b53d75f66b2a9ed07e
jfoppes <jffoppes@gmail.com>
2023-11-03 17:23:11 +0300

    first commit

---
+++ caseStudyCh13.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Foppes, Jacob 
+# CINF 308 Progrmaing for Informatics week 11 -  case study ch 13
+
+
+# For each section of the project i will create a seperate branch with the name of the section  
