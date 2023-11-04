919155a15642456a7b2f16c300cca15229776ff4
jfoppes <jffoppes@gmail.com>
2023-11-04 17:18:39 +0300

    13.2 complete

--- caseStudyCh13.py
+++ caseStudyCh13.py
@@ -16,7 +16,7 @@ leFile = leFile.translate(str.maketrans('','',string.punctuation))
 
 leString = ""
 x = 0
-for line in leFile:
+for line in leFile: # for weach letter i nthe file add it to the string starting after the header that ends at latter 730
     x += 1
     if x <730:
         pass
@@ -24,13 +24,13 @@ for line in leFile:
         leString = leString + line
 
 
-words=(leString.split())
+words=(leString.split())# make a list of all words in book 
 #print("there are",len(words),"words in Yellow wallpaper")
 
 
 freq = {}
 
-for i in words:
+for i in words: # for each word in the words lsit make a dictionary entry if one deosnt exit, increment the value for the word 
     if i in freq:
         freq[i] += 1
     else:
