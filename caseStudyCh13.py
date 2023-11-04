3d5beae7e7c8222713f5124792ae9799e9f68887
jfoppes <jffoppes@gmail.com>
2023-11-04 17:13:58 +0300

    13.5 Complete

--- caseStudyCh13.py
+++ caseStudyCh13.py
@@ -5,9 +5,25 @@
 
 
 # For each section of the project i will create a seperate branch with the name of the section  
+import random
 
-def choose_from_hist():
-    pass
 
-    
+def histogram(s):# histogram fucntion frm section 11
+    d = dict()
+    for c in s:
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    return d
 
+
+def choose_from_hist(hist): # takes a histogram'ed string and returns a reandom letter 
+    keys = list(hist.keys())
+    values = list(hist.values())
+    choice = random.choices(keys,weights=values) # the probalbilty of returning a specific letter is based on how often it occurs in the string
+    return choice
+theword = 'brontosaurus'
+hist = histogram(theword)
+result  = choose_from_hist(hist)
+print(result[0], "has a probability of",hist[result[0]]/len(theword))
