89ef9c15647d68d56bd468fcba1b1f20f8a1fe2e
jfoppes <jffoppes@gmail.com>
2023-11-04 17:13:37 +0300

    13.5 Complete

--- caseStudyCh13.py
+++ caseStudyCh13.py
@@ -6,31 +6,8 @@
 
 # For each section of the project i will create a seperate branch with the name of the section  
 
-import string
-import operator
-
-
-leFile = open("yellow_wallpaper.txt", "r") #open yellow wallpaper
-leFile = leFile.read().lower() #read each line and append to list 
-leFile = leFile.translate(str.maketrans('','',string.punctuation))
-
-leString = ""
-x = 0
-for line in leFile: # for wach line i nthe file starting at character 730 add it to the string
-    x += 1
-    if x <730:
-        pass
-    else:
-        leString = leString + line
-
-
-words=(leString.split()) # create list out of all words
-#print("there are",len(words),"words in Yellow wallpaper")
-
-goodWordList = ["the","with","but","shal","will","love","that","i"]
-didntMakeCut = sorted(list(set(words) - set(goodWordList)))# subtract wors in the good lsit from all words and strop in new list of not good words 
-
-print("these words are no inthe list ;\n",didntMakeCut)
+def choose_from_hist():
+    pass
 
     
 
